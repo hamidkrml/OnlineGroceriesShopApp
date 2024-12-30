fa67c4543027d576134e0d588e3a56c615d3d9d2
hamidkrml <karimlihamid4@gmail.com>
2024-12-30 21:57:30 +0400

    Initial Commit

---
+++ OnlineGroceriesShopApp/OnlineGroceriesShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineGroceriesShopAppApp.swift
+//  OnlineGroceriesShopApp
+//
+//  Created by hamid on 30.12.24.
+//
+
+import SwiftUI
+
+@main
+struct OnlineGroceriesShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
